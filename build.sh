08775de2956de6b17a3f971ddf81e95b7122a723
Boris Rybalkin <ribalkin@gmail.com>
2021-03-31 00:34:56 +0100

    no syncloud deps for now

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 mkdir -p ${DIR}/build/bin
 
-${DIR}/build/python/bin/pip install -r ${DIR}/requirements.txt
+#${DIR}/build/python/bin/pip install -r ${DIR}/requirements.txt
 
 ${DIR}/build/python/bin/pip install -r ${DIR}/build/home-assistant/requirements_all.txt
 ${DIR}/build/python/bin/python ${DIR}/build/home-assistant/setup.py install
