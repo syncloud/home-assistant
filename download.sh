6484647c015a8481c440963aff16a19914007660
Boris Rybalkin <ribalkin@gmail.com>
2021-03-30 07:49:05 +0100

    python3

--- download.sh
+++ download.sh
@@ -10,8 +10,9 @@ mkdir -p ${BUILD_DIR}
 
 cd $BUILD_DIR
 
-wget --progress=dot:giga ${DOWNLOAD_URL}/python-${ARCH}.tar.gz
-tar xf python-${ARCH}.tar.gz
+wget --progress=dot:giga ${DOWNLOAD_URL}/python3-${ARCH}.tar.gz
+tar xf python3-${ARCH}.tar.gz
+mv python3 python
 
 wget --progress=dot:giga ${DOWNLOAD_URL}/nginx-${ARCH}.tar.gz
 tar xf nginx-${ARCH}.tar.gz
