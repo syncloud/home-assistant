12e599358de83b5b3e25eb2870efce8f5ee55b0b
Boris Rybalkin <ribalkin@gmail.com>
2021-03-31 18:41:56 +0100

    no source

--- build.sh
+++ build.sh
@@ -6,5 +6,7 @@ mkdir -p ${DIR}/build/bin
 
 #${DIR}/build/python/bin/pip install -r ${DIR}/requirements.txt
 
-${DIR}/build/python/bin/pip install -r ${DIR}/build/home-assistant/requirements_all.txt
-${DIR}/build/python/bin/python ${DIR}/build/home-assistant/setup.py install
+#${DIR}/build/python/bin/pip install -r ${DIR}/build/home-assistant/requirements_all.txt
+#${DIR}/build/python/bin/python ${DIR}/build/home-assistant/setup.py install
+
+${DIR}/build/python/bin/pip install home-assistant
