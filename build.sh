99f0864ee4058a9451b82c14ae275aa644c0ded3
Boris Rybalkin <ribalkin@gmail.com>
2021-04-03 15:45:21 +0100

    less ibm-watson

--- build.sh
+++ build.sh
@@ -9,6 +9,7 @@ sed -i 's/boto3==.*/boto3==1.10.0/g' ${DIR}/build/home-assistant/requirements_al
 sed -i 's/hangups==.*/hangups==0.4.13 /g' ${DIR}/build/home-assistant/requirements_all.txt
 sed -i 's/websocket-client==.*/websocket-client==0.57.0 /g' ${DIR}/build/home-assistant/requirements_all.txt
 sed -i '/eebrightbox==.*/d' ${DIR}/build/home-assistant/requirements_all.txt
+sed -i '/ibm-watson==.*/d' ${DIR}/build/home-assistant/requirements_all.txt
 ${DIR}/build/python/bin/pip install -r ${DIR}/build/home-assistant/requirements_all.txt
 #${DIR}/build/python/bin/python ${DIR}/build/home-assistant/setup.py install
 
