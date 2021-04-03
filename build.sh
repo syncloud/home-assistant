e2295fb1235fe4a06d4a9f701b63adf7b0860c15
Boris Rybalkin <ribalkin@gmail.com>
2021-04-03 09:39:29 +0100

    fix boto3 dep

--- build.sh
+++ build.sh
@@ -5,9 +5,10 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 mkdir -p ${DIR}/build/bin
 
 ${DIR}/build/python/bin/pip install -r ${DIR}/requirements.txt
-
-#${DIR}/build/python/bin/pip install -r ${DIR}/build/home-assistant/requirements_all.txt
+sed -i 's/boto3==.*/boto3==1.10.0/g' ${DIR}/build/home-assistant/requirements_all.txt
+${DIR}/build/python/bin/pip install -r ${DIR}/build/home-assistant/requirements_all.txt
 #${DIR}/build/python/bin/python ${DIR}/build/home-assistant/setup.py install
 
 ${DIR}/build/python/bin/pip install homeassistant
+
 
