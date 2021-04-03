99b83411e832ee05d30c02fe1e6d51bd28a2727f
Boris Rybalkin <ribalkin@gmail.com>
2021-04-03 10:12:22 +0100

    remove eebrightbox

--- build.sh
+++ build.sh
@@ -6,6 +6,7 @@ mkdir -p ${DIR}/build/bin
 
 ${DIR}/build/python/bin/pip install -r ${DIR}/requirements.txt
 sed -i 's/boto3==.*/boto3==1.10.0/g' ${DIR}/build/home-assistant/requirements_all.txt
+sed -i 's/eebrightbox==.*/d' ${DIR}/build/home-assistant/requirements_all.txt
 ${DIR}/build/python/bin/pip install -r ${DIR}/build/home-assistant/requirements_all.txt
 #${DIR}/build/python/bin/python ${DIR}/build/home-assistant/setup.py install
 
