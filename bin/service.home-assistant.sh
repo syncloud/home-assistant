858d6d2f4b88a24d712d45576ec2e302e254add4
Boris Rybalkin <ribalkin@gmail.com>
2021-04-07 20:03:32 +0100

    fix venv

--- bin/service.home-assistant.sh
+++ bin/service.home-assistant.sh
@@ -5,4 +5,4 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
 export PATH=$PATH:${DIR}/python/bin
 export LD_LIBRARY_PATH=$PATH:${DIR}/python/lib
 source ${DIR}/home-assistant/bin/activate
-exec ${DIR}/python/bin/python -m homeassistant --config $SNAP_DATA
+exec ${DIR}/home-assistant/bin/hass --config $SNAP_DATA
--- build.sh
+++ build.sh
@@ -8,8 +8,11 @@ ${DIR}/build/python/bin/pip install -r ${DIR}/requirements.txt
 cd ${DIR}/build
 ${DIR}/build/python/bin/python -m venv home-assistant
 export LD_LIBRARY_PATH=${DIR}/build/python/lib
+sed -i 's/include-system-site-packages = false/include-system-site-packages = true/g' home-assistant/pyvenv.cfg
 source home-assistant/bin/activate
 cd ${DIR}/build/core-src
 pip install wheel Cython --constraint homeassistant/package_constraints.txt
 pip install -r requirements_all.txt --constraint homeassistant/package_constraints.txt
 python setup.py install
+sed 's|VIRTUAL_ENV=.*|VIRTUAL_ENV=/snap/home-assistant/home-assistant|g' home-assistant/bin/activate
+sed 's|#!.*/bin/python|/snap/home-assistant/current/python/bin/python|g' home-assistant/bin/hass
