9f14b01805b9c2b93f1b25370748cff999f80737
Boris Rybalkin <ribalkin@gmail.com>
2021-04-07 16:00:25 +0100

    fix start

--- bin/service.home-assistant.sh
+++ bin/service.home-assistant.sh
@@ -2,4 +2,7 @@
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
 
-exec ${DIR}/python/bin/python  ${DIR}/home-assistant/bin/hass --config $SNAP_DATA
+export PATH=$PATH:${DIR}/python/bin
+export LD_LIBRARY_PATH=$PATH:${DIR}/python/lib
+source ${DIR}/home-assistant/bin/activate
+exec ${DIR}/python/bin/python -m homeassistant --config $SNAP_DATA
