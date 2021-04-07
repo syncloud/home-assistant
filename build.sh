03618d1a98db9f69df285fb4297c0eb1d23dcf80
Boris Rybalkin <ribalkin@gmail.com>
2021-04-07 20:18:04 +0100

    fix venv shebang

--- build.sh
+++ build.sh
@@ -13,5 +13,5 @@ cd ${DIR}/build/core-src
 pip install wheel Cython --constraint homeassistant/package_constraints.txt
 pip install -r requirements_all.txt --constraint homeassistant/package_constraints.txt
 python setup.py install
-sed 's|VIRTUAL_ENV=.*|VIRTUAL_ENV=/snap/home-assistant/home-assistant|g' home-assistant/bin/activate
-sed 's|#!.*/bin/python|/snap/home-assistant/current/python/bin/python|g' home-assistant/bin/hass
+sed 's|VIRTUAL_ENV=.*|VIRTUAL_ENV=/snap/home-assistant/home-assistant|g' ${DIR}/build/home-assistant/bin/activate
+sed 's|#!.*/bin/python|/snap/home-assistant/current/python/bin/python|g' ${DIR}/build/home-assistant/bin/hass
