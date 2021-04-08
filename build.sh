92fa2a4dc35a0712a162e5ee0d21fa1d74a633cd
Boris Rybalkin <ribalkin@gmail.com>
2021-04-08 21:52:16 +0100

    fix ln

--- build.sh
+++ build.sh
@@ -20,8 +20,8 @@ sed -i 's|VIRTUAL_ENV=.*|VIRTUAL_ENV=/snap/home-assistant/home-assistant|g' ${DI
 sed -i 's|#!.*/bin/python|#!/snap/home-assistant/current/python/bin/python|g' ${DIR}/build/home-assistant/bin/hass
 sed -i 's|home.*|home = /snap/home-assistant/current/python/bin|g' ${DIR}/build/home-assistant/pyvenv.cfg
 rm ${DIR}/build/home-assistant/bin/python
-ln -s ${DIR}/build/home-assistant/python/bin/python /snap/home-assistant/current/python/bin/python
+ln -s /snap/home-assistant/current/python/bin/python ${DIR}/build/home-assistant/python/bin/python
 rm ${DIR}/build/home-assistant/bin/python3
-ln -s ${DIR}/build/home-assistant/python/bin/python /snap/home-assistant/current/python/bin/python3
+ln -s /snap/home-assistant/current/python/bin/python3 ${DIR}/build/home-assistant/python/bin/python
 rm ${DIR}/build/home-assistant/bin/lib64
-ln -s ${DIR}/build/home-assistant/python/lib64 /snap/home-assistant/current/python/lib
+ln -s /snap/home-assistant/current/python/lib ${DIR}/build/home-assistant/python/lib64 
