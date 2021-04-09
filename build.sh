57fd5f86bb5949c201dbd9f6066d2ac9c6663b90
Boris Rybalkin <ribalkin@gmail.com>
2021-04-09 18:13:37 +0100

    fix current

--- build.sh
+++ build.sh
@@ -16,7 +16,7 @@ cd ${DIR}/build/core-src
 pip install wheel Cython --constraint homeassistant/package_constraints.txt
 pip install --no-cache-dir --no-index --only-binary=:all: --find-links ${WHEELS_LINKS} -r requirements_all.txt --constraint homeassistant/package_constraints.txt
 python setup.py install
-sed -i 's|VIRTUAL_ENV=.*|VIRTUAL_ENV=/snap/home-assistant/home-assistant|g' ${DIR}/build/home-assistant/bin/activate
+sed -i 's|VIRTUAL_ENV=.*|VIRTUAL_ENV=/snap/home-assistant/current/home-assistant|g' ${DIR}/build/home-assistant/bin/activate
 find ${DIR}/build/home-assistant -type f -executable -exec sed -i 's|#!.*/bin/python.*|#!/snap/home-assistant/current/python/bin/python|g' {} \;
 sed -i 's|home.*|home = /snap/home-assistant/current/python/bin|g' ${DIR}/build/home-assistant/pyvenv.cfg
 rm ${DIR}/build/home-assistant/bin/python3
