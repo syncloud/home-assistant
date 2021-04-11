f69bbba1c6e799875617fa9a4066eb62407de80f
Boris Rybalkin <ribalkin@gmail.com>
2021-04-11 15:05:45 +0100

    lib64

--- bin/service.home-assistant.sh
+++ bin/service.home-assistant.sh
@@ -3,7 +3,7 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
 
 #export PATH=$PATH:${DIR}/python/bin
-export LD_LIBRARY_PATH=$PATH:${DIR}/python/lib
+export LD_LIBRARY_PATH=$PATH:${DIR}/python/lib:PATH:${DIR}/python/lib64
 source ${DIR}/home-assistant/bin/activate
 #export PYTHONPATH=/snap/home-assistant/current/home-assistant/lib/python3.8/site-packages
 exec python ${DIR}/home-assistant/bin/hass --config $SNAP_DATA
