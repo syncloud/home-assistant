df2dd31e7a81421cd78b64e059bc15449129c633
Boris Rybalkin <ribalkin@gmail.com>
2021-04-12 21:45:40 +0100

    libtool

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 WHEELS_LINKS=https://wheels.home-assistant.io/alpine-3.12/$(dpkg-architecture -q DEB_HOST_ARCH)/
 apt update
-apt install -y libncurses5 libudev-dev build-essential musl cmake
+apt install -y libncurses5 libudev-dev build-essential musl cmake libtool
 ${DIR}/build/python/bin/pip install -r ${DIR}/requirements.txt
 
 cd ${DIR}/build
