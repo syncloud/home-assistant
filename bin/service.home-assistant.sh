8f7d06c72c5c3ed6323b371ed3c945e61c8a44c0
Boris Rybalkin <ribalkin@gmail.com>
2022-07-22 12:14:12 +0100

    module

--- bin/service.home-assistant.sh
+++ bin/service.home-assistant.sh
@@ -2,4 +2,4 @@
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
 
-exec ${DIR}/home-assistant/bin/python ${DIR}/home-assistant/bin/hass --config $SNAP_DATA/ha.config
+exec ${DIR}/home-assistant/bin/python -m homeassistant --config $SNAP_DATA/ha.config
