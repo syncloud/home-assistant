72eb6c36d3f824ddf6ef23b819757b6662f3db7e
Boris Rybalkin <ribalkin@gmail.com>
2022-07-23 11:10:27 +0100

    refresh

--- integration/verify.py
+++ integration/verify.py
@@ -64,9 +64,9 @@ def test_index(app_domain, device_session):
     wait_for_rest(device_session, 'https://{0}'.format(app_domain), 200)
 
 
-def test_refresh(app_archive_path, device_host, device_session, device_password):
+def test_refresh(app_archive_path, device_host, device_session, device_password, domain):
     local_install(device_host, device_password, app_archive_path)
-    wait_for_installer(device_session, device_host)
+    wait_for_installer(device_session, domain)
 
 
 def test_remove(device, app):
