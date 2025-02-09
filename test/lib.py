1b634856fd6a39b94bac69f6cf7b5f3e74d22081
Boris Rybalkin <ribalkin@gmail.com>
2025-02-09 09:36:41 +0000

    var

--- test/lib.py
+++ test/lib.py
@@ -5,9 +5,9 @@ def login(selenium, device_user, device_password):
     selenium.open_app()
     selenium.screenshot('index')
 
-    selenium.find_by(By.XPATH, "//input[@name='username']")
+    username = selenium.find_by(By.XPATH, "//input[@name='username']")
     username.send_keys(device_user)
-    selenium.find_by(By.XPATH, "//input[@name='password']")
+    password = selenium.find_by(By.XPATH, "//input[@name='password']")
     password.send_keys(device_password)
     selenium.screenshot('login-credentials')
     password.send_keys(Keys.RETURN)
